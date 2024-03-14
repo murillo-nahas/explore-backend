b80425f8b9e7ac017be57b9c5c6957bfba778e02
murillo.nahas <murillonahas@hotmail.com>
2024-03-13 23:43:14 -0300

    feat: port 3333 and use StateModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { StateModule } from "./public/state/state.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [StateModule],
+  controllers: [],
 })
 export class AppModule {}
