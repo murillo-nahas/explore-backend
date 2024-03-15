bc1b10c48cfd0e69c1ec54db4cc1e637b47144f5
Murillo Nahás <murillonahas@hotmail.com>
2024-03-15 14:43:23 -0300

    feat(wip): create city operations

---
+++ src/public/city/city.service.ts
@@ -0,0 +1,67 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnprocessableEntityException,
+} from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateCitySchema } from "src/schemas/city/create-city-schema";
+
+@Injectable()
+export class CityService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    const cities = await this.prisma.city.findMany();
+
+    if (!cities) throw new BadRequestException();
+
+    return { cities };
+  }
+
+  async findOne(id: number) {
+    const city = await this.prisma.city.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!city) throw new NotFoundException("Cannot find city.");
+
+    return {
+      city,
+    };
+  }
+
+  async create(body: CreateCitySchema) {
+    const data = await this.prisma.city.create({
+      data: {
+        name: body.name,
+        acronym: body.acronym,
+        zipCode: body.zipCode,
+        stateId: body.stateId,
+      },
+    });
+
+    if (!data) throw new UnprocessableEntityException();
+
+    return { data };
+  }
+
+  async update(id: number, data: CreateCitySchema) {
+    return this.prisma.city.update({ where: { id }, data });
+  }
+
+  async delete(id: number) {
+    const deleteById = this.prisma.city.delete({
+      where: {
+        id,
+      },
+    });
+
+    if (!deleteById) throw new NotFoundException("Cannot find city.");
+
+    return {};
+  }
+}
