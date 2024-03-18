687d128dc59cf4ea5f160c01c643ffef7849a908
Murillo Nahás <murillonahas@hotmail.com>
2024-03-18 19:34:56 -0300

    feat: update place schema

--- src/schemas/place/create-place-schema.ts
+++ src/schemas/place/create-place-schema.ts
@@ -2,13 +2,14 @@ import { z } from "zod";
 
 export const createPlaceBodySchema = z.object({
   name: z.string().min(1),
-  description: z.string().min(1),
-  googleMapsUrl: z.string().url().min(1),
+  overview: z.string().min(1),
+  googleMapsUri: z.string().url().min(1),
   street: z.string().min(1),
-  number: z.number().min(1),
+  number: z.string().min(1),
   district: z.string().min(1),
   zipCode: z.string().min(1).max(8),
-  cityId: z.number().min(1),
+  cityId: z.string().min(1),
+  categoryId: z.string().min(1),
 });
 
 export type CreatePlaceSchema = z.infer<typeof createPlaceBodySchema>;
