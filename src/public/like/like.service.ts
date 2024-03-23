f24c4f3bdab3486a6bd8de652eeeb0345f3109e3
Murillo Nahás <murillonahas@hotmail.com>
2024-03-23 15:32:55 -0300

    feat(wip): add prisma service to like

--- src/public/like/like.service.ts
+++ src/public/like/like.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
-export class LikeService {}
+export class LikeService {
+  constructor(private service: PrismaService) {}
+}
