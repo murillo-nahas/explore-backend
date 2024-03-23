20252b303908a4c4a9c5b96c206c2221b7ffa840
Murillo Nahás <murillonahas@hotmail.com>
2024-03-23 14:46:59 -0300

    feat: create user resource

--- src/public/user/user.controller.ts
+++ src/public/user/user.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Body, Controller, HttpCode, Post, UsePipes } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
+import {
+  CreateUserSchema,
+  createUserBodySchema,
+} from "src/schemas/user/create-user-schema";
 
-@Controller('user')
+@Controller("user")
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly service: UserService) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createUserBodySchema))
+  async create(@Body() body: CreateUserSchema) {
+    return this.service.create(body);
+  }
 }
--- src/public/user/user.service.ts
+++ src/public/user/user.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateUserSchema } from "src/schemas/user/create-user-schema";
+
+import { hash } from "bcryptjs";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(body: CreateUserSchema) {
+    const { password } = body;
+
+    const hashedPassword = await hash(password, 8);
+
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        email: body.email,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (existingUser) {
+      throw new ConflictException("User already exists");
+    }
+
+    return await this.prisma.user.create({
+      data: {
+        email: body.email,
+        firstname: body.firstname,
+        username: body.username,
+        password: hashedPassword,
+        bio: body.bio,
+        avatar: body.avatar,
+      },
+    });
+  }
+}
