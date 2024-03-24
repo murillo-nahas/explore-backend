31fb1d57096efb2ff8c4f363217504847edffa54
Murillo Nahás <murillonahas@hotmail.com>
2024-03-24 15:07:53 -0300

    feat: create likes http methods

--- src/public/like/like.controller.ts
+++ src/public/like/like.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UsePipes,
+} from "@nestjs/common";
 import { LikeService } from "./like.service";
+import { LikeSchema, likeBodySchema } from "src/schemas/like/like-schema";
+import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
 
 @Controller("/like")
 export class LikeController {
   constructor(private service: LikeService) {}
+
+  @Get(":id")
+  async getAllUserLikes(@Param() userId: string) {
+    return this.service.getAllUserLikes(userId);
+  }
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(likeBodySchema))
+  async like(@Body() { placeId, userId }: LikeSchema) {
+    return this.service.like(placeId, userId);
+  }
 }
