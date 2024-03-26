f3ca70445ad37e2b41e1a8dacb067cc900a7d998
Murillo Nahás <murillonahas@hotmail.com>
2024-03-26 15:15:10 -0300

    feat(wip): create auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthenticateSchema } from "src/schemas/auth/authenticate-schema";
+import { compare } from "bcryptjs";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+
+  async authenticate(body: AuthenticateSchema) {
+    const { email, password } = body;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user)
+      throw new UnauthorizedException("User credentials do not match.");
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid)
+      throw new UnauthorizedException("User credentials do not match.");
+
+    const accessToken = this.jwt.sign({ sub: user.id });
+
+    return {
+      access_token: accessToken,
+    };
+  }
+}
