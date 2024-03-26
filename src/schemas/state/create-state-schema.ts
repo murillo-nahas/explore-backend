0a58162312d8bd279c846df0a5c9143f4487b727
Murillo Nahás <murillonahas@hotmail.com>
2024-03-26 08:16:01 -0300

    feat: add error message to state schema

--- src/schemas/state/create-state-schema.ts
+++ src/schemas/state/create-state-schema.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const createStateBodySchema = z.object({
-  name: z.string().min(1),
-  country: z.string().min(1),
-  acronym: z.string().min(1),
+  name: z.string().min(4, { message: "Must be 4 or more characters long" }),
+  country: z.string().min(3, { message: "Must be 3 characters or more long " }),
+  acronym: z.string().min(2, { message: "Must be 2 or more characters long" }),
 });
 
 export type CreateStateSchema = z.infer<typeof createStateBodySchema>;
