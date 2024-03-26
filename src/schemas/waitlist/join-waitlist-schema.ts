fa48ac605cec39bcb7012d397fbb016444a190c4
Murillo Nahás <murillonahas@hotmail.com>
2024-03-26 08:17:46 -0300

    feat: add error message to waitlist schema

--- src/schemas/waitlist/join-waitlist-schema.ts
+++ src/schemas/waitlist/join-waitlist-schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const joinWaitlistBodySchema = z.object({
-  email: z.string().email().min(1),
-  firstname: z.string().min(1),
+  email: z.string().email({ message: "Invalid e-mail address" }),
+  firstname: z.string().min(1, { message: "Firstname is required" }),
 });
 
 export type JoinWaitlistSchema = z.infer<typeof joinWaitlistBodySchema>;
