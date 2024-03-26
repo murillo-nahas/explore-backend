16fa28a2bb4e71d3c1d77594b75f62431725f368
Murillo Nahás <murillonahas@hotmail.com>
2024-03-26 08:19:28 -0300

    feat: add error message to user schema

--- src/schemas/user/create-user-schema.ts
+++ src/schemas/user/create-user-schema.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const createUserBodySchema = z.object({
-  email: z.string().email().min(1),
-  firstname: z.string().min(1),
-  username: z.string().min(1),
-  password: z.string().min(1),
-  bio: z.string().min(1),
-  avatar: z.string().min(1),
+  email: z.string().email({ message: "Invalid e-mail address" }),
+  firstname: z.string().min(1, { message: "Firstname is required" }),
+  username: z.string().min(1, { message: "Username is required" }),
+  password: z.string().min(1, { message: "Password is required" }),
+  bio: z.string().min(10, { message: "Must be 10 characters or more long" }),
+  avatar: z.string().min(1, { message: "Avatar is required" }),
 });
 
 export type CreateUserSchema = z.infer<typeof createUserBodySchema>;
