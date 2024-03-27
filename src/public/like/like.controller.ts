15fd2ea396f41b0016160a9bd53f7754e0921159
Murillo Nahás <murillonahas@hotmail.com>
2024-03-27 14:15:47 -0300

    feat: add auth guard to like

--- src/public/like/like.controller.ts
+++ src/public/like/like.controller.ts
@@ -5,22 +5,26 @@ import {
   HttpCode,
   Param,
   Post,
+  UseGuards,
   UsePipes,
 } from "@nestjs/common";
 import { LikeService } from "./like.service";
 import { LikeSchema, likeBodySchema } from "src/schemas/like/like-schema";
 import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 
 @Controller("/like")
 export class LikeController {
   constructor(private service: LikeService) {}
 
   @Get(":id")
+  @UseGuards(JwtAuthGuard)
   async getAllUserLikes(@Param() userId: string) {
     return this.service.getAllUserLikes(userId);
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(likeBodySchema))
   async like(@Body() { placeId, userId }: LikeSchema) {
